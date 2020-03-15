5e771e8aafefa15afc39f4c9683d832c553f11b2
12157591+AplUSAndmINUS@users.noreply.github.com <terence.waters@imail.org>
2020-03-15 14:21:21 -0600

    fixed errors with spacing and clear button

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,70 @@
 import React from 'react';
+import { Button, Col, Container, Form, FormGroup, Jumbotron, Row } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 const App = () => {
-  const [state, setState] = React.useState({
-    celsius: ''
-  });
-  const [result, setResult] = React.useState(0);
+	const [state, setState] = React.useState({
+		celsius: ''
+	});
+	const [result, setResult] = React.useState('');
 
-  const handleCelsius = (evt: any) => {
-    setState({ celsius: evt.target.value });
-  };
+	const handleCelsius = (evt: any) => {
+		setState({ celsius: evt.target.value });
+	};
 
-  const celsiusToFahrenheit = () => {
-    const celsius = parseInt(state.celsius);
-    setResult(celsius * 1.8 + 32);
-  };
+	const celsiusToFahrenheit = ($e: any) => {
+    $e.preventDefault();
+		const celsius = parseInt(state.celsius);
+		setResult(`Fahrenheit: ${(celsius * 1.8 + 32).toString()}\xB0`);
+	};
 
-  return (
-    <div className='container-fluid p-3 my-3'>
-      <h1>Convert Celsius to Fahrenheit</h1>
-      <form onSubmit={celsiusToFahrenheit}>
-        <div className='form-group'>
-          <label htmlFor='celsius'>Celsius:</label>
-          <input
-            type='number'
-            className='form-control'
-            value={state.celsius}
-            name='celsius'
-            onChange={handleCelsius}
-          />
-        </div>
-        <button type='submit' className='btn btn-primary'>
-          Submit
-        </button>
-        <button type='reset' className='btn btn-default'>
-          Clear
-        </button>
-      </form>
-      <div className='container-fluid'>
-        <div className='row'>
-          <h5 id='result'>Fahrenheit: {result}</h5>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<Jumbotron>
+				<h1>Convert Celsius to Fahrenheit</h1>
+				<p>This application takes a Celsius number and converts it to Fahrenheit</p>
+			</Jumbotron>
+			<Container fluid>
+				<Row>
+					<Col md={6}>
+						<Form onSubmit={celsiusToFahrenheit}>
+							<FormGroup>
+								<Form.Label htmlFor='celsius'>Celsius:</Form.Label>
+								<Form.Control
+									type='number'
+									value={state.celsius}
+									name='celsius'
+									onChange={handleCelsius}
+								/>
+							</FormGroup>
+							<Button type='submit' className='primary'>
+								Submit
+							</Button>{' '}
+							<Button
+								type='reset'
+								variant='outline-secondary'
+								onClick={() => {
+									setState({
+										celsius: ''
+									});
+									setResult('');
+								}}
+							>
+								Clear
+							</Button>
+						</Form>
+					</Col>
+				</Row>
+        <p>&nbsp;</p>
+				<Row>
+          <Col>
+            <h5 id='result'>{result}</h5>
+          </Col>
+        </Row>
+			</Container>
+		</>
+	);
 };
 
 export default App;
